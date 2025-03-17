781ae24111870857ff35c77b6c6677aab64fbbd2
teron131 <t3ron.ay@gmail.com>
2025-03-17 07:26:04 +0000

    Import env

--- langchain_playground/__init__.py
+++ langchain_playground/__init__.py
@@ -2,9 +2,13 @@
 LangChain Playground - A collection of LangChain utilities and tools
 """
 
+from dotenv import load_dotenv
+
 from .SmolagentsHF import UniversalAgent
 from .Tools import get_tools
 from .universal import UniversalChain
+
+load_dotenv()
 
 __all__ = ["get_tools", "UniversalChain", "UniversalAgent"]
 
